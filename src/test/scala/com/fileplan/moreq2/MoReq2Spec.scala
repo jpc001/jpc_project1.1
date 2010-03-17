0134de8258a5c615d202dec5d6b1ff942b0619c0
Ben Johnson <ben.johnson@jandpconsulting.co.uk>
2010-03-17 23:11:25 +1100

    Rebuilt project and classpath files.

--- src/test/scala/com/fileplan/moreq2/MoReq2Spec.scala
+++ src/test/scala/com/fileplan/moreq2/MoReq2Spec.scala
@@ -3,8 +3,18 @@ package com.fileplan.moreq2
 import org.scalatest.FeatureSpec
 import org.scalatest.GivenWhenThen
 
-/** Scala 2.8 requires ScalaTest 1.0.1
+/** ScalaTest using Scala 2.8 requires ScalaTest version 1.0.1
  *  http://www.scala-tools.org/repo-snapshots/org/scalatest/scalatest/1.0.1-for-scala-2.8.0.Beta1-RC1-SNAPSHOT/
+ *  
+ *  Note that running "buildr eclipse" from the command line will generate a .classpath and
+ *  .project files that conform to the BuildR structure.  However, any artifacts, including
+ *  ScalaTest, will be defined with a classpath of "M2_REPO/...".  This variable must be 
+ *  defined in Eclipse under Configure Build Path -> Add Variable -> M2_REPO and set it to:
+ *  
+ *  	<user home directory>/.m2/repository
+ *  
+ * For example, "E:/Users/Ben/.m2/repository".  Failure to do this will mean Eclipse cannot
+ * find the artifacts and report compile errors.
  */
 class MoReq2Spec extends FeatureSpec {
 
